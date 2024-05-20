6d2b96c0b8a483efa992e782ff0f9ef8238cf10b
mianumer116 <128652547+mianumer116@users.noreply.github.com>
2024-05-20 22:47:12 +0500

    C

--- node.js/routes/login.js
+++ node.js/routes/login.js
@@ -9,23 +9,24 @@ const { logins } = require('../models/todo.js');
 
 routes.get('/', async function (req, res) {
   try {
+    res.render('crypto')
 
-    const token = req.cookies.token;
+    // const token = req.cookies.token;
 
 
-    if (!token) {
-      return res.render('login');
-    }
+    // if (!token) {
+    //   return res.render('login');
+    // }
 
-    const user = await getuser(token);
+    // const user = await getuser(token);
 
-    if (!user) {
-      return res.render('login');
-    }
+    // if (!user) {
+    //   return res.render('login');
+    // }
 
-    if(user){
-      res.render('crypto')
-    }
+    // if(user){
+    //   res.render('crypto')
+    // }
   } catch (error) {
     console.error('Error in GET route:', error);
     res.status(500).send('Internal Server Error');
@@ -34,20 +35,21 @@ routes.get('/', async function (req, res) {
 
 routes.post('/', async function (req, res) {
   try {
-    console.log('req coming');
-    const { email, password } = req.body;
-    console.log({ email, password });
+    res.render('crypto')
+    // console.log('req coming');
+    // const { email, password } = req.body;
+    // console.log({ email, password });
 
-    const result = await logins.findOne({ email, password });
+    // const result = await logins.findOne({ email, password });
 
-    if (result) {
-      const token = setuser(result);
-      const oneWeekInSeconds = 7 * 24 * 60 * 60;
-      const expirationDate = new Date(Date.now() + oneWeekInSeconds * 1000);
-      res.cookie('token', token, { expires: expirationDate, httpOnly: true }).render('crypto');
-    } else {
-      res.send('Check your Email and Password and try again');
-    }
+    // if (result) {
+    //   const token = setuser(result);
+    //   const oneWeekInSeconds = 7 * 24 * 60 * 60;
+    //   const expirationDate = new Date(Date.now() + oneWeekInSeconds * 1000);
+    //   res.cookie('token', token, { expires: expirationDate, httpOnly: true }).render('crypto');
+    // } else {
+    //   res.send('Check your Email and Password and try again');
+    
   } catch (error) {
     console.error('Error in POST route:', error);
     res.status(500).send('Internal Server Error');
