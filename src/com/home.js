56a2e6d7faded201bbcaec741c71d705c14d4a36
mianumer116 <128652547+mianumer116@users.noreply.github.com>
2024-06-23 15:45:22 +0500

    c

--- src/com/home.js
+++ src/com/home.js
@@ -3,13 +3,14 @@ import Footer from './footer'
 import {Link} from 'react-router-dom'
 import Msgs from './msgs'
 
+
 function Home(){
     return(
         <>
         <div className="container home">
             <div className="row row-home">
                 <div className="col-md-5 offset-md-1">
-                    <img  className="home-img" src="https://scontent.flhe13-1.fna.fbcdn.net/v/t39.30808-6/211099031_343009547342438_8151184923034008264_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=5f2048&_nc_ohc=JlT87xUJwMUQ7kNvgHP6YVu&_nc_ht=scontent.flhe13-1.fna&oh=00_AYBLIXE4zRdiNZUMvXyxbkD1ODD6ftqP0ZuA8F7RRYBxQg&oe=666EA5FE"/>
+                    <img  className="home-img" src="https://scontent.flhe13-1.fna.fbcdn.net/v/t39.30808-6/448733224_1018008389842547_5690376814240670044_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeFC2TNvqd8q512ubUhatBMdKk2SeAaVWXUqTZJ4BpVZdUEZUxog2oCAj3cqKjAXx32rag9YpGsF3HDNmycIqr9T&_nc_ohc=3c5xkvJFxFEQ7kNvgFvwLfJ&_nc_ht=scontent.flhe13-1.fna&oh=00_AYDu-2mnCsmzsmgrkxwjKeCJwn0KS41iaO2SjR7n4qlNWQ&oe=667DBA26"/>
 
                 </div>
 
